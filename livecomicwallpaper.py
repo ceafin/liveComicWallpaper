fd5123a9e1a015f696d5e68296a62fd6bff4aef1
Ceafin <jasmilner@gmail.com>
2019-02-17 13:33:11 -0600

    initial files, etc

---
+++ livecomicwallpaper.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+"""A short description of the module -- called a docstring."""
+
+# Here comes your imports
+# Here comes your (few) global variables
+# Here comes your class definitions
+# Here comes your function definitions
+
+
+def main():
+    print("Hello, World!")
+    return 0
+
+
+if __name__ == "__main__":
+    main()
